f6a5e84ad227f7a44c945377beff742b81827c7d
simoneborin <borin.simone1997@gmail.com>
2024-12-19 12:34:41 +0100

    theme provider

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import './styles/globals.css';
 import { AuthProvider } from './context/AuthContext';
-
+import './styles/index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
-        <AppRoutes />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </ThemeProvider>
     </AuthProvider>
   );
 };
