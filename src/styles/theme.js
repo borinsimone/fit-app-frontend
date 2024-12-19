7daacde2cc1407053d428cf763ec2fa16605a688
simoneborin <borin.simone1997@gmail.com>
2024-12-19 12:34:26 +0100

    theme e general style

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+// theme.js
+const theme = {
+  colors: {
+    light: '#00C6BE',
+    background: '#16181F',
+    secondaryBg: '#d9d9d910',
+    text: '#d9d9d9',
+  },
+  fonts: {
+    primary: 'Arial, sans-serif',
+    secondary: 'Georgia, serif',
+  },
+  spacing: {
+    small: '8px',
+    medium: '16px',
+    large: '32px',
+  },
+};
+
+export default theme;
