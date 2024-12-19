0d520869d98bd4edd471e893b69979fee7dca0c5
simoneborin <borin.simone1997@gmail.com>
2024-12-20 00:10:53 +0100

    fetch workout on component mount

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getWorkouts } from '../services/workoutService';
+import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../context/GlobalContext';
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const { workouts, setWorkouts } = useGlobalContext();
+  const navigate = useNavigate();
+  // Fetch workouts on component mount and update workouts on token change or login/logout
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        navigate('/login');
+        return;
+      }
+      console.log('start fetching');
+
+      const workoutsData = await getWorkouts(token);
+      setWorkouts(workoutsData);
+    };
+
+    fetchWorkouts();
+  }, [navigate]);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          getWorkouts();
+        }}
+      >
+        getworkouts
+      </button>
+      {workouts?.map((workout) => (
+        <div>{workout.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default DashboardPage;
