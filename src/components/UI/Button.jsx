0a863246f373051a57302bdbcb6853c72fd7fbb4
simoneborin <borin.simone1997@gmail.com>
2024-12-19 12:33:47 +0100

    button input components ui

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,14 +1,24 @@
+// components/ui/Button.jsx
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = ({ children, onClick, type = 'button' }) => {
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+// Definisci un componente stilizzato
+const StyledButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.light};
+  color: ${({ theme }) => theme.colors.background};
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  font-weight: bold;
+
+  &:hover {
+    /* background-color: #45a049; */
+  }
+`;
+
+const Button = ({ children, onClick }) => {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 };
 
 export default Button;
---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,30 @@
+// components/ui/Input.jsx
+import React from 'react';
+import styled from 'styled-components';
+
+// Definisci un componente stilizzato per l'input
+const StyledInput = styled.input`
+  padding: 10px;
+  color: ${({ theme }) => theme.colors.text};
+  background-color: ${({ theme }) => theme.colors.secondaryBg};
+  border: 1px solid ${({ theme }) => theme.colors.light};
+  box-shadow: 0 0 30px #00000075;
+  border-radius: 10px;
+
+  &:focus {
+    border-color: #4caf50;
+  }
+`;
+
+const Input = ({ type, placeholder, value, onChange }) => {
+  return (
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
