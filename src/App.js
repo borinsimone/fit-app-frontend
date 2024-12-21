4c80881b9048dca8f7d4416b8b41126493f8251d
simoneborin <borin.simone1997@gmail.com>
2024-12-21 18:12:23 +0100

    fetch workout + workout form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import { HashRouter as Router, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  HashRouter as Router,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import './styles/globals.css';
 import { AuthProvider } from './context/AuthContext';
 import './styles/index.css';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
-import { GlobalProvider } from './context/GlobalContext';
+import { GlobalProvider, useGlobalContext } from './context/GlobalContext';
 import BottomBar from './components/Dashboard/BottomBar';
+import { jwtDecode } from 'jwt-decode';
+import { getWorkouts } from './services/workoutService';
+import WorkoutForm from './assets/workout/WorkoutForm';
 
 const AppContent = () => {
+  const { workouts, setWorkouts, user, setUser, workoutFormOpen } =
+    useGlobalContext();
+  const navigate = useNavigate();
   const location = useLocation();
   const hiddenRoutes = ['/login', '/register'];
   const showBottomBar = !hiddenRoutes.includes(location.pathname);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const fetchWorkouts = async () => {
+      if (!token) {
+        navigate('/login');
+        return;
+      }
+      console.log('start fetching');
+
+      const workoutsData = await getWorkouts(token);
+      setWorkouts(workoutsData);
+    };
+
+    fetchWorkouts();
+    try {
+      const decodedToken = jwtDecode(token); // Decodifica il token
+      setUser(decodedToken); // Salva i dati nel state
+    } catch (error) {
+      console.error('Errore nella decodifica del token', error);
+    }
+  }, []);
   return (
     <>
       <AppRoutes />
       {showBottomBar && <BottomBar />}
+      {workoutFormOpen && <WorkoutForm />}
     </>
   );
 };
