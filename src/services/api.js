abca5cb9131e8fdec81052c9d4b1a8499e7d0c90
simoneborin <borin.simone1997@gmail.com>
2024-12-22 23:25:19 +0100

    x

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+const API_URL = 'https://fit-app-backend-babz.onrender.com';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 export const fetchApi = async (endpoint, options) => {
   const response = await fetch(`${API_URL}${endpoint}`, options);
