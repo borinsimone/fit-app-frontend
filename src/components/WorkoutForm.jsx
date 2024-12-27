7b727fc19c4c77c62961606052860d95e7f8560c
simoneborin <borin.simone1997@gmail.com>
2024-12-27 10:37:28 +0100

    form exercise choices

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { MdClose, MdDelete } from 'react-icons/md';
-import { addWorkouts } from '../../services/workoutService';
-import { useGlobalContext } from '../../context/GlobalContext';
+import { addWorkouts } from '../services/workoutService';
+import { useGlobalContext } from '../context/GlobalContext';
 
 function WorkoutForm({}) {
   const { setWorkoutFormOpen, workoutDateBuffer } = useGlobalContext();
@@ -144,6 +144,102 @@ function WorkoutForm({}) {
     console.log(workout);
     addWorkouts(workout);
   };
+  const exercises = {
+    riscaldamento: [
+      'Corsa leggera sul tapis roulant',
+      'Cyclette a bassa intensità',
+      'Ellittica a bassa intensità',
+      'Jumping jacks',
+      'Skip (salto con ginocchia alte)',
+      'Marcia sul posto',
+      'Mobilizzazione articolare (spalle, anche, caviglie)',
+      'Stretching dinamico (gambe, braccia, schiena)',
+      'Rotazioni del busto',
+      'Affondi a passo lento',
+    ],
+    petto: [
+      'Panca piana con bilanciere',
+      'Panca inclinata con bilanciere',
+      'Panca piana con manubri',
+      'Panca inclinata con manubri',
+      'Croci su panca piana',
+      'Croci su panca inclinata',
+      'Chest press',
+      'Push-up (flessioni)',
+      'Dips alle parallele (petto)',
+      'Pullover con manubrio',
+    ],
+    schiena: [
+      'Trazioni alla sbarra',
+      'Lat machine avanti',
+      'Lat machine dietro',
+      'Rematore con bilanciere',
+      'Rematore con manubrio',
+      'Rematore con cavo basso',
+      'Pulldown con cavi',
+      'Stacchi da terra',
+      'Hyperextension (iperestensioni)',
+      'Face pull',
+    ],
+    spalle: [
+      'Lento avanti con bilanciere',
+      'Lento avanti con manubri',
+      'Arnold press',
+      'Alzate laterali con manubri',
+      'Alzate frontali con manubri',
+      'Scrollate per trapezi',
+      'Military press',
+      'Tirate al mento con bilanciere',
+      'Reverse fly (posterior deltoid)',
+    ],
+    gambe: [
+      'Squat con bilanciere',
+      'Leg press',
+      'Affondi con manubri',
+      'Affondi con bilanciere',
+      'Stacco rumeno',
+      'Hack squat',
+      'Estensioni delle gambe (leg extension)',
+      'Leg curl (flessione delle gambe)',
+      'Calf raise (sollevamento polpacci)',
+    ],
+    bicipiti: [
+      'Curl con bilanciere',
+      'Curl con manubri',
+      'Curl con cavo',
+      'Hammer curl',
+      'Concentration curl',
+      'Preacher curl',
+    ],
+    tricipiti: [
+      'French press',
+      'Push-down con cavo',
+      'Estensioni sopra la testa con manubrio',
+      'Dips alle parallele (tricipiti)',
+      'Diamond push-up',
+      'Kickback con manubri',
+    ],
+    addominali: [
+      'Crunch a terra',
+      'Crunch su fitball',
+      'Plank',
+      'Plank laterale',
+      'Russian twist',
+      'Leg raise (sollevamento gambe)',
+      'Mountain climber',
+      'Bicycle crunch',
+      'Ab wheel rollout',
+      'V-up',
+    ],
+    cardio: [
+      'Corsa sul tapis roulant',
+      'Ellittica',
+      'Cyclette',
+      'Rowing machine (vogatore)',
+      'Salto con la corda',
+      'HIIT (High-Intensity Interval Training)',
+    ],
+  };
 
   return (
     <FormContainer onSubmit={handleSubmit}>
@@ -211,6 +307,7 @@ function WorkoutForm({}) {
                   <Label>Nome Esercizio:</Label>
                   <Input
                     type='text'
+                    list={`exercise-options-${sectionIndex}-${exerciseIndex}`}
                     value={exercise.name}
                     onChange={(e) =>
                       handleExerciseChange(sectionIndex, exerciseIndex, {
@@ -219,7 +316,20 @@ function WorkoutForm({}) {
                       })
                     }
                   />
+                  <datalist
+                    id={`exercise-options-${sectionIndex}-${exerciseIndex}`}
+                  >
+                    {Object.values(exercises)
+                      .flat()
+                      .map((exerciseName, idx) => (
+                        <option
+                          key={idx}
+                          value={exerciseName}
+                        />
+                      ))}
+                  </datalist>
                 </FormGroup>
+
                 <FormGroup>
                   <Label>Time Based:</Label>
                   <Input
